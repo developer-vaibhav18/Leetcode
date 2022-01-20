1b7fbfa7e3e53231b90e985e710a3b20122a0cc5
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-01-20 15:31:33 +0530

    Time: 4 ms (77.76%), Space: 6.9 MB (87.35%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int c=0,ans=0,i,mn=s.size(),mx=-1,cur=0,n=s.size();
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        
+        for(i=0;i<n;i++){
+            if(s[i]=='('){
+                c++;
+                // if(dp[c]<=mx){
+                    dp[c]=i;
+                // }
+            }
+            else if(s[i]==')'){
+                c--;
+                if(c>=0){
+                    if(dp[c]>=mx)
+                        ans=max(ans,i-dp[c]);
+                }
+                else{
+                    c=0;
+                    dp[0]=i;
+                    mx=i;
+                }
+            }
+        }
+        return ans;
+    }
+};
