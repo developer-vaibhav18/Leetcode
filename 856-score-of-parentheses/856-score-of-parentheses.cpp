a620e6f467b2eac20e9a83f8afee24313187c4e3
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-01-23 10:51:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (97.95%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    string s;
+    int rec(int &i){
+        int c=0,ans=0;
+        while(i<s.size()){
+            if(s[i]=='('){
+                if(s[i+1]==')'){
+                    ans+=1;
+                    i+=2;
+                    if(i<s.size() && s[i]=='('){
+                        return ans+rec(i);
+                    }
+                    return 1;
+                }
+                i++;c++;
+                ans+=(2*rec(i));
+            }
+            else{
+                if(i+1<s.size() && s[i+1]=='('){
+                    i++;
+                    return ans+rec(i);
+                }
+                i++;
+                return ans;
+            }
+        }
+        return ans;
+    }
+public:
+    int scoreOfParentheses(string s) {
+        this->s=s;
+        int i=0;
+        return rec(i);
+    }
+};
