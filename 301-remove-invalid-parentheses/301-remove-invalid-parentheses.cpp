840bbb75a45e76387059e78246d9418d0dd1e5c0
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-01-23 09:47:15 +0530

    Time: 192 ms (38.70%), Space: 8.7 MB (75.76%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -0,0 +1,80 @@
+class Solution {
+private:
+    bool valid(string &s){
+        int c=0,i,n=s.size();
+        for(i=0;i<n;i++){
+            if(s[i]=='('){
+                c++;
+            }
+            else if(s[i]==')'){
+                c--;
+                if(c<0)return false;
+            }
+        }
+        return c==0;
+    }
+    set<string> ans;
+    int mx;
+    string s;
+    void rec(int i,char last,stack<int> &st,string &cur){
+        if(i==s.size()){
+            // cout<<cur<<"\n";
+            if(cur.size()==mx && valid(cur))ans.insert(cur);
+            return;
+        }
+        if(s[i]!='(' && s[i]!=')'){
+            cur+=s[i];
+            rec(i+1,last,st,cur);
+            cur.pop_back();
+            return;
+        }
+        // cout<<s[i]<<" "<<
+        rec(i+1,s[i],st,cur);
+        if(s[i]==')'){
+            if(!st.empty()){
+                // if(last!=')'){
+                    int t=st.top();st.pop();
+                    cur+=s[i];
+                    rec(i+1,last,st,cur);
+                    cur.pop_back();
+                    st.push(t);
+                // }
+            }
+            return;
+        }
+        // if(last!='('){
+            st.push(i);
+            cur+=s[i];
+            rec(i+1,last,st,cur);
+            cur.pop_back();
+            st.pop();
+        // }
+    }
+public:
+    vector<string> removeInvalidParentheses(string str) {
+        int n=str.size(),c=0,i;
+        s=str;
+        mx=0;
+        for(i=0;i<n;i++){
+            if(str[i]=='('){
+                c++;
+            }
+            else if(str[i]==')'){
+                if(c){
+                    mx+=2;
+                    c--;
+                }
+            }
+            else{
+                mx++;
+            }
+        }
+        // cout<<mx<<"\n";
+        stack<int> st;
+        string cur="";
+        rec(0,'a',st,cur);
+        return vector(ans.begin(),ans.end());
+    }
+};
+
+// ())))(((()
