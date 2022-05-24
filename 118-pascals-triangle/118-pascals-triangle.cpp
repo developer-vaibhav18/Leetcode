c9bfa0bac11493e908df24012217248429492805
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-05-25 00:23:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.68%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -6,7 +6,7 @@ public:
         ans[0]={1};
         for(int i=1;i<n;i++){
             ans[i].push_back(1);
-            for(int j=1;j<ans[i-1].size();j++){
+            for(int j=1;j<i;j++){
                 ans[i].push_back(ans[i-1][j]+ans[i-1][j-1]);
             }
             ans[i].push_back(1);
