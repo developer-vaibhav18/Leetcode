7b7036683b69956d09597fffc389248980ce70e8
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-06-23 11:13:53 +0530

    Time: 9 ms (57.71%), Space: 10.6 MB (70.35%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
-    void helper(vector<vector<int>> &res, vector<int> &candidates, vector<int> &cur, int tar, int idx , bool lastTaken){
-        if(tar == 0){
-            res.push_back(cur);
-            return;
-        }
-        
-        if(idx >= candidates.size() || tar<0){
-            return;
-        }
-        
-        if(lastTaken || (candidates[idx] != candidates[idx - 1])){
-            cur.push_back(candidates[idx]);
-            helper(res, candidates, cur, tar - candidates[idx], idx + 1, true);
-            cur.pop_back();
-        }
-        
-        helper(res, candidates, cur, tar, idx + 1, false);
-    }
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
+    vector<vector<int>> combinationSum2(vector<int>& v, int tar) {
+        sort(v.begin(),v.end());
         vector<vector<int>> res;
-        vector<int> cur;
-        helper(res, candidates, cur, target, 0, 1);
+        vector<int> t;
+        aux(v,res,t,tar);
         return res;
+    }
+private:
+    void aux(vector<int> &v,vector<vector<int>> &res,vector<int> &t,int tar,int i=0){
+        if(tar==0){
+            res.push_back(t);
+            return;
+        }
+        int j;
+        for(j=i;j<v.size();j++){
+            if(tar-v[j]<0){
+                return;
+            }
+            if(j!=i && v[j]==v[j-1]){
+                continue;
+            }
+            t.push_back(v[j]);
+            aux(v,res,t,tar-v[j],j+1);
+            t.pop_back();
+        }
     }
 };
