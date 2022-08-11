0c225d66470a4ae3c68b87ced47a1b74a5e44af1
Vaibhav Kumar Sinha <67749908+developer-vaibhav18@users.noreply.github.com>
2022-08-11 15:55:03 +0530

    Time: 206 ms (25.77%), Space: 13.4 MB (60.31%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,35 +8,57 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-struct compare {
-    bool operator()(const ListNode* l, const ListNode* r) {
-        return l->val > r->val;
+// struct compare {
+//     bool operator()(const ListNode* l, const ListNode* r) {
+//         return l->val > r->val;
+//     }
+// };
+ListNode* merge(ListNode *l1, ListNode *l2) {
+    if (l1 == NULL) return l2;
+    if (l2 == NULL) return l1;
+    
+    if (l1->val < l2->val) {
+        l1->next = merge(l1->next, l2);
+        return l1;
     }
-};
+    l2->next = merge(l1, l2->next);
+    return l2;
+}
+
+ListNode* aux(vector<ListNode*>& lists, ListNode *par, int cur) {
+    if (cur == lists.size()) return par;
+    if (lists[cur] == NULL) return aux(lists, par, cur + 1);
+    
+    par = merge(par, lists[cur]);
+    
+    return aux(lists, par, cur + 1);
+}
 
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.size()==0)return NULL;
-        priority_queue<ListNode*, vector<ListNode*>, compare>pq;
+        return aux(lists, lists[0], 1);
+//         priority_queue<ListNode*, vector<ListNode*>, compare>pq;
         
-        for (auto list : lists) {
-            if (list)
-                pq.push(list);
-        }
+//         for (auto list : lists) {
+//             if (list)
+//                 pq.push(list);
+//         }
         
-        ListNode *dummy = new ListNode(-1), *cur = dummy;
+//         ListNode *dummy = new ListNode(-1), *cur = dummy;
         
-        while (!pq.empty()) {
-            ListNode *t = pq.top();
-            pq.pop();
-            cur->next = t;
-            cur = cur->next;
+//         while (!pq.empty()) {
+//             ListNode *t = pq.top();
+//             pq.pop();
+//             cur->next = t;
+//             cur = cur->next;
             
-            if (t->next) {
-                pq.push(t->next);
-            }
-        }
-        return dummy->next;
+//             if (t->next) {
+//                 pq.push(t->next);
+//             }
+//         }
+//         return dummy->next;
+//     }
     }
 };
